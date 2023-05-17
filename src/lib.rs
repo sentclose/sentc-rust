ab26d3434e8972099ef31abf6254554d48b3fe2b
Jörn Heinemann <joernheinemann@gmx.de>
2023-05-17 21:58:41 +0200

    init

---
+++ src/lib.rs
@@ -0,0 +1,12 @@
+//! # Sentc sdk for rust clients
+//!
+//! Handles user, groups and keys + the encryption and decryption.
+//!
+//! Work in progress
+
+#![no_std]
+
+pub fn start() -> &'static str
+{
+	"Work in progress"
+}
